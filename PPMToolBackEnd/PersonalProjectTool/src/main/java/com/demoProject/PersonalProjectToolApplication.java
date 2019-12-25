a96799ebc6b882927df6617c6bd639372208e71a
priyo153 <priyojit.chatterjee333@gmail.com>
2019-12-25 15:29:28 +0530

    started working on backend

---
+++ PPMToolBackEnd/PersonalProjectTool/src/main/java/com/demoProject/PersonalProjectToolApplication.java
@@ -0,0 +1,13 @@
+package com.demoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectToolApplication.class, args);
+	}
+
+}
