b67acfbd43e4758be2b1d9913127fb4cdbcfaa22
priyo153 <priyojit.chatterjee333@gmail.com>
2020-01-02 09:50:59 +0530

    refactored frontend

--- ppm-tool-front-end/src/actions/projectActions.js
+++ ppm-tool-front-end/src/actions/projectActions.js
@@ -3,7 +3,7 @@ import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("http://localhost:8080/projects", project);
+    const res = await axios.post("/projects", project);
     history.push("/dashboard");
     dispatch({
       type: GET_ERRORS,
@@ -19,7 +19,7 @@ export const createProject = (project, history) => async dispatch => {
 
 export const updateProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.put("http://localhost:8080/projects", project);
+    const res = await axios.put("/projects", project);
     history.push("/dashboard");
   } catch (err) {
     dispatch({
@@ -30,7 +30,7 @@ export const updateProject = (project, history) => async dispatch => {
 };
 
 export const getProjects = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/projects");
+  const res = await axios.get("/projects");
   dispatch({
     type: GET_PROJECTS,
     payload: res.data
@@ -39,7 +39,7 @@ export const getProjects = () => async dispatch => {
 
 export const getProject = (id, history) => async dispatch => {
   try {
-    const res = await axios.get(`http://localhost:8080/projects/${id}`);
+    const res = await axios.get(`/projects/${id}`);
     dispatch({
       type: GET_PROJECT,
       payload: res.data
@@ -50,9 +50,15 @@ export const getProject = (id, history) => async dispatch => {
 };
 
 export const deleteProject = id => async dispatch => {
-  await axios.delete(`http://localhost:8080/projects/${id}`);
-  dispatch({
-    type: DELETE_PROJECT,
-    payload: id
-  });
+  if (
+    window.confirm(
+      `Are you sure you want to delete the project with ID ${id}   ? `
+    )
+  ) {
+    await axios.delete(`/projects/${id}`);
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: id
+    });
+  }
 };
